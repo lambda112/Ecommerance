e531466f8568af34c5913f4deb44b56d5c66635e
Kamran Azim <Kamazim121212@gmail.com>
2024-01-09 23:38:39 +0000

    Created Function To Get Item Names
    Function navigates thorugh two divs a h4 and anchor in order to get all item names this is passed through a list comprehension where the title attribute is grabbed

--- laptops.py
+++ laptops.py
@@ -3,3 +3,7 @@ from selectolax.parser import HTMLParser
 
 resp = r.get("https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops")
 tree = HTMLParser(resp.content)
+
+def get_item_name():
+    name = [i.attrs["title"] for i in tree.css("div div h4 > a")]
+    return name
